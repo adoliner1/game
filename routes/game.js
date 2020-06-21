28bd6c6bb583eba6becb3e0d6d535b08a10128ab
Henry Phu <hphu@bu.edu>
2020-06-21 12:57:21 -0500

    no need to copy paste each row

--- routes/game.js
+++ routes/game.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('game', { title: 'Game' });
+  res.render('game', { title: 'Game', boardLength: 15, boardWidth: 9 });
 });
 
 router.get('/*', function(req, res, next) {
-  res.render('game', { title: 'Game' });
+  res.render('game', { title: 'Game', boardLength: 15, boardWidth: 9 });
 });
 
 
