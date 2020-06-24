c4d9a0c9ce8b23dd5f4f431132f38a221bd777fb
adoliner1 <adoliner@gmail.com>
2020-06-24 16:49:30 -0400

    reorganizing lobby functionality and game making functionality to separate files

--- public/javascripts/game/constants.js
+++ public/javascripts/game/constants.js
@@ -1,15 +1,32 @@
-const boardLength = 15;
-const boardWidth = 9;
-const startOfRedTiles = 0;
-const endOfRedTiles = 3;
-const startOfBlueTiles = 12;
-const endOfBlueTiles = 15;
+const boardLength = 15
+const boardWidth = 9
+const startOfRedTiles = 0
+const endOfRedTiles = 3
+const startOfBlueTiles = 12
+const endOfBlueTiles = 15
 
-module.exports = {
-  boardLength,
-  boardWidth,
-  startOfRedTiles,
-  endOfRedTiles,
-  startOfBlueTiles,
-  endOfBlueTiles
+//move to game maker constants?
+const blueHQTile = {col: 4, row: 13}
+const redHQTile = {col: 4, row: 1}
+const centerVPTokensTile = {col: 4, row: 7}
+const intialBlueBuilder1Tile = {col: 2, row: 12}
+const intialBlueBuilder2Tile = {col: 6, row: 12}
+const intialRedBuilder1Tile = {col: 2, row: 2}
+const intialRedBuilder2Tile = {col: 6, row: 2}
+
+module.exports = 
+{
+	boardLength,
+	boardWidth,
+	startOfRedTiles,
+	endOfRedTiles,
+	startOfBlueTiles,
+	endOfBlueTiles,
+	blueHQTile,
+	redHQTile,
+	centerVPTokensTile,
+	intialBlueBuilder1Tile,
+	intialBlueBuilder2Tile,
+	intialRedBuilder1Tile,
+	intialRedBuilder2Tile
 };
